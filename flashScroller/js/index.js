3684de007841b7b1c97295e2fe1f8f8d3fe28e5a
马宁 <ma_ning@corp.netease.com>
2018-04-24 11:05:13 +0800

    add

--- flashScroller/js/index.js
+++ flashScroller/js/index.js
@@ -2,21 +2,22 @@ $(function(){
     init();
 })
 
-let scrollPro =0;
+let scrollPro =0,movie=0;
 
 function scrollInit(){
 	scroller = new Scroller(function(left,top,zoom){
 		console.log(scrollPro);
-        // scrollPro = parseInt(top);
-        scrollPro = (top);
-        exportRoot.gotoAndStop(scrollPro);
+        scrollPro = (top/8);//减慢滑动
+        if(scrollPro!=0){
+           exportRoot.main.gotoAndStop(scrollPro); 
+        }
+
 	},{
         zooming: true,
         bouncing: false
     })
-
-    contentLength = exportRoot.totalFrames-1;
-    scroller.options.animationDuration=150;
+    contentLength = (exportRoot.main.totalFrames-1)*8;
+    console.log((exportRoot.main.totalFrames-1)*8)
 	scroller.setDimensions(stage.width, stage.height, stage.height, contentLength);
 	
 	//添加触屏事件
@@ -42,4 +43,30 @@ function scrollInit(){
         scroller.doTouchEnd(e.timeStamp);
         mousedown = false;
     }, false);
+}
+
+// 音乐播放
+function music(s){
+    var flag = false;
+    $(document).bind('touchstart', function(){
+        if (!flag) {
+          $('#mp3')[0].play();
+          $('.music').addClass("play");
+          flag = true;
+        }
+    });
+    $('.music').click(function(){
+      if(!$('#mp3')[0].paused){
+           $('#mp3')[0].pause();
+           $('.music').removeClass("play");
+        }else{
+            $('#mp3')[0].play();
+            $('.music').addClass("play");
+        }
+    });
+}
+
+function stopMusic(){
+    $('#mp3')[0].pause();
+    $('.music').removeClass("play");
 }
