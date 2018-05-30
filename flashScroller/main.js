3cc42eb9a675d2ab9576b0cceb52d83aeb2d55a4
马宁 <ma_ning@corp.netease.com>
2018-05-30 16:58:58 +0800

    删除index 里面的audio标签

--- flashScroller/main.js
+++ flashScroller/main.js
@@ -67,6 +67,73 @@ p.nominalBounds = new cjs.Rectangle(-1,-1,1238,642);
 	// actions tween:
 	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
 
+	// 图层_3
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#00FF00").s().p("AhIAuQgDgTAAgVQADhzBMgFQAwABASA0IgGABQgPgvgtgBQhGAEgDBuIAAAQQARg2A3gBQBDAFAGBEQgEBHhHAEQg7AAgOhFgAhBAuQAKBAA4AAQBBgDAEhDQgFg+g+gFQhAACgEBHg");
+	this.shape.setTransform(1645.9,-77.2);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#00FF00").s().p("AhMABQAFhPBHgGQBLAEACBUIiTAAQAGBJBAADQA3gBANgqIAGACQgPAtg7ABQhIgFgEhPgAhGgBICNAAQgEhMhDgBQhAAFgGBIg");
+	this.shape_1.setTransform(1618.1,-74.2);
+
+	this.shape_2 = new cjs.Shape();
+	this.shape_2.graphics.f("#00FF00").s().p("AA7BUIAAhhQAAhBg3AAQg6AGgFBOIAABOIgFAAIAAilIAFAAIAAAzQATg0AsgBQA+AAgBBGIAABhg");
+	this.shape_2.setTransform(1598.7,-74.3);
+
+	this.shape_3 = new cjs.Shape();
+	this.shape_3.graphics.f("#00FF00").s().p("AhOABQAIhOBGgIQBIAIAGBOQgFBOhJAGQhHgGgHhOgAhHABQAFBJBCAFQBCgEAHhKQgHhJhCgHQhCAHgFBJg");
+	this.shape_3.setTransform(1579.4,-74.1);
+
+	this.shape_4 = new cjs.Shape();
+	this.shape_4.graphics.f("#00FF00").s().p("AA7ByIAAhjQAAg/g1AAQg8AGgFBOIAABOIgGAAIAAjjIAGAAIAABxQATg0AugCQA7ABAABEIAABjg");
+	this.shape_4.setTransform(1560,-77.2);
+
+	this.shape_5 = new cjs.Shape();
+	this.shape_5.graphics.f("#00FF00").s().p("AhFBwIAAjdIAGAAIAAAwQATgxAtgBQBBAFAEBSQgCBShCABQgugBgTgxIAABngAg/gYQAFBJA8AEQA9gBABhMQgEhMg7gFQg8AFgEBMg");
+	this.shape_5.setTransform(1541.4,-71.5);
+
+	this.shape_6 = new cjs.Shape();
+	this.shape_6.graphics.f("#00FF00").s().p("AgCBzIAAimIAFAAIAACmgAgKhuIAFgEIAQAaIgFADg");
+	this.shape_6.setTransform(1529,-77.3);
+
+	this.shape_7 = new cjs.Shape();
+	this.shape_7.graphics.f("#FFFFFF").s().p("AAAABIhZByIgFgEIBchxIhUhsIAEgEIBSBrIBShrIAFAEIhTBsIBbBxIgFAEg");
+	this.shape_7.setTransform(1820,-68.9);
+
+	this.shape_8 = new cjs.Shape();
+	this.shape_8.graphics.f("#FFFFFF").s().p("AhMABQAFhPBHgGQBLAEACBUIiTAAQAGBJBAADQA3gBANgqIAGACQgPAtg7ABQhIgFgEhPgAhGgBICNAAQgEhMhDgBQhAAFgGBIg");
+	this.shape_8.setTransform(1800.1,-65.9);
+
+	this.shape_9 = new cjs.Shape();
+	this.shape_9.graphics.f("#FFFFFF").s().p("AA8BUIAAhhQAAhBg3AAQg7AGgFBOIAABOIgFAAIAAilIAFAAIAAAzQATg0AtgBQA8AAAABGIAABhg");
+	this.shape_9.setTransform(1780.7,-66);
+
+	this.shape_10 = new cjs.Shape();
+	this.shape_10.graphics.f("#FFFFFF").s().p("AhOABQAIhOBGgIQBHAIAIBOQgHBOhIAGQhHgGgHhOgAhIABQAGBIBCAHQBDgFAFhKQgFhJhDgHQhCAHgGBJg");
+	this.shape_10.setTransform(1761.4,-65.8);
+
+	this.shape_11 = new cjs.Shape();
+	this.shape_11.graphics.f("#FFFFFF").s().p("AA8ByIAAhjQAAg/g2AAQg8AGgFBOIAABOIgFAAIAAjjIAFAAIAABxQATg0AugCQA8ABgBBEIAABjg");
+	this.shape_11.setTransform(1742,-68.9);
+
+	this.shape_12 = new cjs.Shape();
+	this.shape_12.graphics.f("#FFFFFF").s().p("AhFBwIAAjdIAGAAIAAAwQATgxAtgBQBBAFAEBSQgCBShCABQgugBgTgxIAABngAg/gYQAFBJA8AEQA9gBABhMQgEhMg7gFQg8AFgEBMg");
+	this.shape_12.setTransform(1723.4,-63.2);
+
+	this.shape_13 = new cjs.Shape();
+	this.shape_13.graphics.f("#FFFFFF").s().p("AgCBzIAAimIAFAAIAACmgAgKhuIAFgEIAQAaIgFADg");
+	this.shape_13.setTransform(1711,-69);
+
+	this.shape_14 = new cjs.Shape();
+	this.shape_14.graphics.f().s("#FFFFFF").ss(1,1,1).p("EhgpgATMDBUAAn");
+	this.shape_14.setTransform(1257.7,-38.2);
+
+	this.shape_15 = new cjs.Shape();
+	this.shape_15.graphics.f().s("#99FF00").ss(1,1,1).p("EhQjAAAMChHAAA");
+	this.shape_15.setTransform(1154.7,-113.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_15},{t:this.shape_14},{t:this.shape_13},{t:this.shape_12},{t:this.shape_11},{t:this.shape_10},{t:this.shape_9},{t:this.shape_8},{t:this.shape_7},{t:this.shape_6},{t:this.shape_5},{t:this.shape_4},{t:this.shape_3},{t:this.shape_2},{t:this.shape_1},{t:this.shape}]}).wait(1));
+
 	// 图层_2
 	this.main = new lib.元件1();
 	this.main.name = "main";
@@ -76,17 +143,17 @@ p.nominalBounds = new cjs.Rectangle(-1,-1,1238,642);
 	this.timeline.addTween(cjs.Tween.get(this.main).wait(1));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(959.1,514,1238,642);
+p.nominalBounds = new cjs.Rectangle(958.1,400.5,1239.3,755.6);
 // library properties:
 lib.properties = {
 	id: 'A6884732E68D459480F25FAFF18F0553',
 	width: 640,
 	height: 1030,
 	fps: 24,
-	color: "#FFFFFF",
+	color: "#000000",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/Bitmap2.png?1527668136331", id:"Bitmap2"}
+		{src:"images/Bitmap2.png?1527670629687", id:"Bitmap2"}
 	],
 	preloads: []
 };
